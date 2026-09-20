789b61881542315c27339f6eeab2c849899273ae
angel3122004 <angelmary23cs@cce.edu.in>
2026-09-20 16:35:40 +0530

    Update display_usedcar_gradio.py

--- display_usedcar_gradio.py
+++ display_usedcar_gradio.py
@@ -24,4 +24,9 @@ app = gr.Interface(
 )
 
 
-app.launch(server_name="0.0.0.0", server_port=7860)
+import os
+
+app.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
